fd2d2538b62ec5a7f5810d53676e6f2ab1d2570a
doff6501 <61034864+doff6501@users.noreply.github.com>
2020-03-05 19:20:53 -0600

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        DatingProfile Ted = new DatingProfile("Ted", "Dancing", 42, "Male");
+        Ted.WriteBio("Loves the OutDoors");
+
+        DatingProfile Babes = new DatingProfile("Babes", "Bunny", 39, "Female");
+        Babes.WriteBio("Loves To Hike and Bike Ride");
+
+        Ted.SendMessage("Hello Babes", "Wanna Take a Walk With Me",Babes);
+        Babes.ReadMessage();
+        }
+    }
+
